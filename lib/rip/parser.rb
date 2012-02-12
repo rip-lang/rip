0fc49fbf5d0b6174ad6e89c8c83eda443bd39d14
Raving Genius <rg+code@ravinggenius.com>
2012-02-12 16:17:59 -0500

    Parse simple literals

--- lib/rip/parser.rb
+++ lib/rip/parser.rb
@@ -5,14 +5,89 @@ module Rip
   class Parser < Parslet::Parser
     root(:statements)
 
-    rule(:statement) { comment }
+    rule(:statement) { comment | expression >> spaces.maybe >> comment.maybe }
     rule(:statements) { statement.repeat }
 
     rule(:comment) { (str('#') >> (eol.absent? >> any).repeat.as(:comment)) >> eol.maybe }
 
+    rule(:expression) { simple_expression }
+    rule(:expression_terminator) { str(';') | eol }
+
+    rule(:simple_expression) { simple_expression_fancy >> spaces.maybe >> expression_terminator.maybe }
+    rule(:simple_expression_fancy) { object }
+
     #---------------------------------------------
 
+    rule(:object) { simple_object }
+
+    rule(:simple_object) { nil_literal | boolean | numeric | character | string | regular_expression }
+
+    #---------------------------------------------
+
+    rule(:nil_literal) { str('nil').as(:nil) }
+
+    rule(:boolean) { true_literal | false_literal }
+
+    rule(:true_literal) { str('true').as(:true) }
+
+    rule(:false_literal) { str('false').as(:false) }
+
+    #---------------------------------------------
+
+    # WARNING order is important here: decimal must be before integer or the integral part of a decimal could be interpreted as a integer followed by a decimal starting with a '.' (dot)
+    rule(:numeric) { decimal | integer }
+
+    rule(:decimal) { (sign.maybe >> digits.maybe >> str('.') >> digits).as(:decimal) }
+
+    rule(:integer) { (sign.maybe >> digits).as(:integer) }
+
+    rule(:sign) { match['+-'] }
+
+    rule(:digit) { match['0-9'] }
+
+    # allow _ to be used to group digits ( 3_423_752 )
+    # _ may not come first or last
+    rule(:digits) { digit.repeat(1) >> (str('_').maybe >> digit.repeat(1)).repeat }
+
+    #---------------------------------------------
+
+    # FIXME should match any single printable unicode character
+    rule(:character) { str('`') >> match['0-9a-zA-Z_'].as(:character) }
+
+    #---------------------------------------------
+
+    # NOTE a string is just a list with characters allowed in it
+    rule(:string) { symbol_string | single_quoted_string | double_quoted_string | here_doc}
+
+    # FIXME should match most printable characters except whitespace
+    rule(:symbol_string) { str(':') >> match['a-zA-Z_'].repeat(1).as(:string) }
+
+    rule(:single_quoted_string) { str('\'') >> (str('\'').absent? >> any).repeat.as(:string) >> str('\'') }
+
+    rule(:double_quoted_string) { str('"') >> (str('"').absent? >> any).repeat.as(:string) >> str('"') }
+
+    rule(:here_doc) do
+      label = match['A-Z_'].repeat(1)
+      start = str('<<') >> label.as(:here_doc_start) >> eol
+      content = (label.absent? >> any).repeat.as(:string)
+      finish = label.as(:here_doc_end) >> eol.maybe
+      start >> content >> finish
+    end
+
+    #---------------------------------------------
+
+    # TODO expand regular expression pattern
+    rule(:regular_expression) { str('/') >> (str('/').absent? >> any).repeat.as(:regex) >> str('/') }
+
+    #---------------------------------------------
+
+    rule(:whitespace) { spaces | eols }
+
+    rule(:space) { str(' ') | str("\t") }
+    rule(:spaces) { space.repeat }
+
     rule(:eol) { str("\r\n") | str("\n") | str("\r") }
+    rule(:eols) { eol.repeat }
 
     #---------------------------------------------
 
---
+++ tests/rip/parser_simple_literal_test.rb
@@ -0,0 +1,59 @@
+require_relative '../test_case'
+
+class ParserSimpleLiteralTest < TestCase
+  def test_nil_literal
+    n = parser.nil_literal.parse('nil')
+    assert_equal 'nil', n[:nil]
+  end
+
+  def test_true_literal
+    t = parser.boolean.parse('true')
+    assert_equal 'true', t[:true]
+
+    f = parser.boolean.parse('false')
+    assert_equal 'false', f[:false]
+  end
+
+  def test_numeric
+    integer = parser.numeric.parse('42')
+    assert_equal '42', integer[:integer]
+
+    decimal = parser.numeric.parse('4.2')
+    assert_equal '4.2', decimal[:decimal]
+
+    negative = parser.numeric.parse('-3')
+    assert_equal '-3', negative[:integer]
+
+    long = parser.numeric.parse('123_456_789')
+    assert_equal '123_456_789', long[:integer]
+  end
+
+  def test_character
+    character = parser.character.parse('`f')
+    assert_equal 'f', character[:character]
+  end
+
+  def test_string
+    symbol_string = parser.string.parse(':one')
+    assert_equal 'one', symbol_string[:string]
+
+    single_string = parser.string.parse('\'two\'')
+    assert_equal 'two', single_string[:string]
+
+    double_string = parser.string.parse('"three"')
+    assert_equal 'three', double_string[:string]
+
+    rip_doc = <<-RIP_DOC
+<<HERE_DOC
+here docs are good for multi-line strings
+HERE_DOC
+    RIP_DOC
+    here_doc = parser.string.parse(rip_doc)
+    assert_equal "here docs are good for multi-line strings\n", here_doc[:string]
+  end
+
+  def test_regular_expression
+    regex = parser.regular_expression.parse('/hello/')
+    assert_equal 'hello', regex[:regex]
+  end
+end
