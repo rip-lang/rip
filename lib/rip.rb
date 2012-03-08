a3a24b43b47d026ba394d8048e7ac9f28591259d
Raving Genius <rg+code@ravinggenius.com>
2012-03-08 00:26:37 -0500

    Implement Rip.{project_path,root}

--- lib/rip.rb
+++ lib/rip.rb
@@ -1,2 +1,11 @@
+require 'pathname'
+
 module Rip
+  def self.project_path
+    Pathname('.').expand_path
+  end
+
+  def self.root
+    Pathname File.expand_path('..', __FILE__)
+  end
 end
--- tests/rip_test.rb
+++ tests/rip_test.rb
@@ -1,4 +1,13 @@
 require_relative 'test_case'
 
+require 'pathname'
+
 class RipTest < TestCase
+  def test_project_path
+    assert_equal Pathname(Dir.pwd).expand_path, Rip.project_path
+  end
+
+  def test_root
+    assert_equal Pathname('lib').expand_path, Rip.root
+  end
 end
