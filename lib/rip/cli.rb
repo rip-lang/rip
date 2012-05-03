5db3df42eaf17ac22cf51e75ec590e9a3a9cbf51
Raving Genius <rg+code@ravinggenius.com>
2012-05-03 18:52:34 -0400

    Fill in Rip::CLI a bit

--- lib/rip/cli.rb
+++ lib/rip/cli.rb
@@ -2,8 +2,9 @@ require 'pathname'
 
 require 'rip/parser'
 
-# $ rip <file>
-# $ rip run <file>
+# $ rip [run] <file>
+# $ rip [repl]
+# $ rip do [command] [arguments...]
 # $ rip help [command]
 # $ rip parse_tree <file>
 # $ rip syntax_tree <file>
@@ -24,8 +25,29 @@ module Rip
     end
 
     def parse_tree(file)
-      tree = Rip::Parser.new.parse_file Pathname(file).expand_path
-      puts tree
+      puts make_parse_tree(file)
+    end
+
+    def syntax_tree(file)
+      puts make_syntax_tree(file)
+    end
+
+    def do(command, *args)
+      make_syntax_tree(file).walk
+    end
+
+    def repl
+      # TODO enter repl
+      puts 'repl command not implemented yet'
+    end
+
+    # TODO enter repl if file is nil
+    def run(file = nil)
+      if file
+        make_syntax_tree(file).walk
+      else
+        repl
+      end
     end
 
     def self.start
@@ -37,6 +59,16 @@ module Rip
       else
         cli.help
       end
+    end
+
+    protected
+
+    def make_parse_tree(filename)
+      Rip::Parser.new.parse_file(Rip.project_path.join(filename).expand_path)
+    end
+
+    def make_syntax_tree(filename)
+      make_parse_tree(filename).to_ast
     end
   end
 end
