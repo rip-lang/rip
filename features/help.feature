6c3fdc8245862e479f200970049d386d390e4513
Raving Genius <rg+code@ravinggenius.com>
2012-08-22 19:43:23 -0400

    Remove unimplemented feature

--- features/help.feature
+++ features/help.feature
@@ -7,7 +7,3 @@ Feature: Getting help from the command line
   Scenario: General help with expected flag
     Given I run `rip --help`
     Then the output should contain a brief explanation for everything
-
-  Scenario: Specific help
-    Given I run `rip help repl`
-    Then the output should contain detailed help for running the REPL
--- features/step_definitions/help_steps.rb
+++ features/step_definitions/help_steps.rb
@@ -9,7 +9,3 @@ Then /^the output should contain a brief explanation for everything$/ do
     output.should match(/^  \[--#{option}\] + # .{10,}$/), "A description for `--#{option}` should probably be longer than ten characters to be useful"
   end
 end
-
-Then /^the output should contain detailed help for running the REPL$/ do
-  pending # express the regexp above with the code you wish you had
-end
