37d9a4b742c59550a464227208dbe64421d24a4e
Raving Genius <rg+code@ravinggenius.com>
2012-09-26 17:46:46 -0400

    Move project tests to rspec

---
+++ spec/rip_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Rip do
+  describe '.project_path' do
+    its(:project_path) { should eq(Pathname(Dir.pwd).expand_path) }
+  end
+
+  # describe '.project_path=' do
+  #   around(:each) do |it_block|
+  #     default_path = Rip.project_path
+  #     Rip.project_path = 'some/other/directory'
+  #     it_block.call
+  #     Rip.project_path = default_path
+  #   end
+
+  #   its(:project_path) { should eq(Pathname('some/other/directory').expand_path) }
+  # end
+
+  describe '.root' do
+    its(:root) { should eq(Pathname('lib').expand_path) }
+  end
+end
