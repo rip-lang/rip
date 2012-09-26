e667444a6d365620458484f13b58fd0a9c03c8bf
Raving Genius <rg+code@ravinggenius.com>
2012-09-26 17:49:07 -0400

    Implement Rip.project_path=

--- lib/rip.rb
+++ lib/rip.rb
@@ -2,12 +2,12 @@ require 'pathname'
 
 module Rip
   def self.project_path
-    Pathname('.').expand_path
+    Pathname(@path || '.').expand_path
   end
 
-  #def self.project_path=(path)
-  #  @path = path
-  #end
+  def self.project_path=(path)
+    @path = path
+  end
 
   def self.root
     Pathname File.expand_path('..', __FILE__)
--- spec/rip_spec.rb
+++ spec/rip_spec.rb
@@ -5,16 +5,16 @@ describe Rip do
     its(:project_path) { should eq(Pathname(Dir.pwd).expand_path) }
   end
 
-  # describe '.project_path=' do
-  #   around(:each) do |it_block|
-  #     default_path = Rip.project_path
-  #     Rip.project_path = 'some/other/directory'
-  #     it_block.call
-  #     Rip.project_path = default_path
-  #   end
+  describe '.project_path=' do
+    around(:each) do |it_block|
+      default_path = Rip.project_path
+      Rip.project_path = 'some/other/directory'
+      it_block.call
+      Rip.project_path = default_path
+    end
 
-  #   its(:project_path) { should eq(Pathname('some/other/directory').expand_path) }
-  # end
+    its(:project_path) { should eq(Pathname('some/other/directory').expand_path) }
+  end
 
   describe '.root' do
     its(:root) { should eq(Pathname('lib').expand_path) }
