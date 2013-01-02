8850b2d0935e87ac4bddd1da03454414c00587c4
Raving Genius <rg+code@ravinggenius.com>
2013-01-02 00:00:58 -0500

    General code cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,9 @@ require_relative '../lib/rip/parser'
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-  config.filter_run :focus
+  config.filter_run :focus => true
+  config.filter_run_excluding :blur => true
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
