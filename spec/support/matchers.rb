3304c24adf9d9afb4fef3fdee37e5f2d82a80e5c
Raving Genius <rg+code@ravinggenius.com>
2013-03-30 20:13:07 -0400

    Tweak error messages for :parse_as and :parse_first_as

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -1,30 +1,44 @@
 RSpec::Matchers.define :parse_as do |expected|
   match do |parser|
+    @source_code = parser.source_code
     @actual = parser.parse_tree
     @actual == expected
   end
 
   failure_message_for_should do |actual|
-    "expected parse_tree to be #{expected}, but was #{@actual} instead"
+    strip_heredoc(<<-MESSAGE)
+      Expected parse tree to be #{expected}, but was #{@actual} instead. Source was:
+      #{@source_code}
+    MESSAGE
   end
 
   failure_message_for_should_not do |actual|
-    "expected parse_tree to not be #{expected}, but it was"
+    strip_heredoc(<<-MESSAGE)
+      Expected parse tree to not be #{expected}, but it was. Source was:
+      #{@source_code}
+    MESSAGE
   end
 end
 
 RSpec::Matchers.define :parse_first_as do |expected|
   match do |parser|
+    @source_code = parser.source_code
     @actual = parser.parse_tree.first
     @actual == expected
   end
 
   failure_message_for_should do |actual|
-    "expected parse_tree.first to be #{expected}, but was #{@actual} instead"
+    strip_heredoc(<<-MESSAGE)
+      Expected first leaf in parse tree to be #{expected}, but was #{@actual} instead. Source was:
+      #{@source_code}
+    MESSAGE
   end
 
   failure_message_for_should_not do |actual|
-    "expected parse_tree.first to not be #{expected}, but it was"
+    strip_heredoc(<<-MESSAGE)
+      Expected first leaf in parse tree to not be #{expected}, but it was. Source was:
+      #{@source_code}
+    MESSAGE
   end
 end
 
