4b656dd00906a68c4a4a3e4e2e3148215b0bf669
Raving Genius <rg+code@ravinggenius.com>
2013-03-31 19:54:58 -0400

    Rip::CLI#{parse_tree,syntax_tree} to allow reading from standard in

--- lib/rip/cli.rb
+++ lib/rip/cli.rb
@@ -41,26 +41,40 @@ Usage:
       puts Rip::Version.to_s(options[:verbose])
     end
 
-    desc 'parse_tree <file>', 'Print the parse tree for <file> and exit'
-    def parse_tree(file)
-      wip :parse_tree
+    desc 'parse_tree [file]', 'Print the parse tree for [file] (or standard in) and exit'
+    def parse_tree(file = nil)
       puts make_parse_tree(file).inspect
     end
 
-    desc 'syntax_tree <file>', 'Print the syntax tree for <file> and exit'
-    def syntax_tree(file)
-      wip :syntax_tree
+    desc 'syntax_tree [file]', 'Print the syntax tree for [file] (or standard in) and exit'
+    def syntax_tree(file = nil)
       puts make_syntax_tree(file).inspect
     end
 
     protected
 
-    def make_parse_tree(filename)
-      Rip::Compiler::Parser.new.parse_file(Rip.project_path.join(filename).expand_path)
+    def load_source_code(origin)
+      resolve_origin(origin).read
     end
 
-    def make_syntax_tree(filename)
-      Rip::Compiler::AST.new(make_parse_tree(filename))
+    def resolve_origin(origin)
+      if origin.nil?
+        STDIN
+      else
+        Rip.project_path.join(origin).expand_path
+      end
+    end
+
+    def make_parser(origin)
+      Rip::Compiler::Parser.new(resolve_origin(origin), load_source_code(origin))
+    end
+
+    def make_parse_tree(origin)
+      make_parser(origin).parse_tree
+    end
+
+    def make_syntax_tree(origin)
+      make_parser(origin).syntax_tree
     end
 
     def wip(command)
