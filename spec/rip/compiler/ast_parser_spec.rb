5b397a19acabe880ae1ab5f896b6893abe5a8a2c
Raving Genius <rg+code@ravinggenius.com>
2013-04-04 00:45:27 -0400

    Refactor syntax tree tests a bit

--- spec/rip/compiler/ast_parser_spec.rb
+++ spec/rip/compiler/ast_parser_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Rip::Compiler::AST do
   let(:location) { location_for }
+  let(:expressions) { syntax_tree(rip).expressions }
 
   context 'some basics' do
     describe 'tree for empty module' do
@@ -10,7 +11,7 @@ describe Rip::Compiler::AST do
 
       specify do
         expect(syntax_tree(rip)).to eq(rip_module)
-        expect(syntax_tree(rip).expressions.count).to eq(0)
+        expect(expressions.count).to eq(0)
       end
     end
 
@@ -19,11 +20,9 @@ describe Rip::Compiler::AST do
       let(:comment) { Rip::Nodes::Comment.new(location.add_character, ' this is a comment') }
       let(:rip_module) { Rip::Nodes::Module.new(location, [ comment ]) }
 
-      let(:actual_comment) { syntax_tree(rip).expressions.first }
-
       specify do
-        expect(syntax_tree(rip).expressions.count).to eq(1)
-        expect(actual_comment).to eq(comment)
+        expect(expressions.count).to eq(1)
+        expect(expressions.first).to eq(comment)
       end
     end
   end
