27a24d6e51b4d6c0eace03413fa48691a15f39ca
Raving Genius <rg+code@ravinggenius.com>
2013-04-26 13:03:54 -0400

    Refactor Rip::Nodes::Try to accept catches and finally

--- lib/rip/nodes/try.rb
+++ lib/rip/nodes/try.rb
@@ -1,15 +1,21 @@
 module Rip::Nodes
   class Try < Base
-    attr_reader :body
+    attr_reader :attempt_body
+    attr_reader :catch_blocks
+    attr_reader :finally_block
 
-    def initialize(location, body)
+    def initialize(location, attempt_body, catch_blocks, finally_block)
       super(location)
-      @body = body
+      @attempt_body = attempt_body
+      @catch_blocks = catch_blocks
+      @finally_block = finally_block
     end
 
     def ==(other)
       super &&
-        (body == other.body)
+        (attempt_body == other.attempt_body) &&
+        (catch_blocks == other.catch_blocks) &&
+        (finally_block == other.finally_block)
     end
   end
 end
