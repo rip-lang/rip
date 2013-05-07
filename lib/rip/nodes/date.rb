b16d8b5d679f79f8c9a3f6ff1c8ad6c063c99c69
Raving Genius <rg+code@ravinggenius.com>
2013-05-06 20:14:10 -0400

    Transform Rip::Nodes::{Date,Time,DateTime}

---
+++ lib/rip/nodes/date.rb
@@ -0,0 +1,27 @@
+module Rip::Nodes
+  class Date < Base
+    attr_reader :year
+    attr_reader :month
+    attr_reader :day
+
+    def initialize(location, year, month, day)
+      super(location)
+      @year = year
+      @month = month
+      @day = day
+    end
+
+    def ==(other)
+      super &&
+        (year == other.year) &&
+        (month == other.month) &&
+        (day == other.day)
+    end
+
+    def to_debug(level = 0)
+      [
+        [ level, "#{super.last.last} (#{year}-#{month}-#{day})" ]
+      ]
+    end
+  end
+end
