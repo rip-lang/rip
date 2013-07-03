872437501948c957048aedffb24fa0893f3bdaf8
Raving Genius <rg+code@ravinggenius.com>
2013-07-02 23:36:16 -0400

    Outdent Rip::Utilities::Scope
    Conflicts:
    	lib/rip/utilities/scope.rb

--- lib/rip/utilities/scope.rb
+++ lib/rip/utilities/scope.rb
@@ -1,38 +1,36 @@
-module Rip
-  module Utilities
-    class Scope
-      attr_reader :state
-      attr_reader :outer_scope
+module Rip::Utilities
+  class Scope
+    attr_reader :state
+    attr_reader :outer_scope
 
-      def initialize(state = {}, outer_scope = nil)
-        @state = state
-        @outer_scope = outer_scope
-      end
+    def initialize(state = {}, outer_scope = nil)
+      @state = state
+      @outer_scope = outer_scope
+    end
 
-      def get(key)
-        state[key] || (outer_scope ? outer_scope.get(key) : nil)
-      end
+    def get(key)
+      state[key] || (outer_scope ? outer_scope.get(key) : nil)
+    end
 
-      def set(key, value)
-        if state.key?(key)
-          location = key.location if key.respond_to?(:location)
-          raise Rip::Exceptions::CompilerException.new("#{key} has already been defined.", location, caller)
-        else
-          self.class.new(state.merge(key => value), outer_scope)
-        end
+    def set(key, value)
+      if state.key?(key)
+        location = key.location if key.respond_to?(:location)
+        raise Rip::Exceptions::CompilerException.new("#{key} has already been defined.", location, caller)
+      else
+        self.class.new(state.merge(key => value), outer_scope)
       end
+    end
 
-      def new(key, value)
-        self.class.new({}, self)
-      end
+    def new(key, value)
+      self.class.new({}, self)
+    end
 
-      def context
-        outer_context.merge(state)
-      end
+    def context
+      outer_context.merge(state)
+    end
 
-      def outer_context
-        outer_scope ? outer_scope.context : {}
-      end
+    def outer_context
+      outer_scope ? outer_scope.context : {}
     end
   end
 end
