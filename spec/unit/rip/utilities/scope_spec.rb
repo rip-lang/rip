c4b5b774dbc0f63f3d2ea5ecf0cd50f6671c2496
Thomas Ingram <code@ravinggenius.com>
2013-12-05 19:42:55 -0500

    Refactor Rip::Utilities::Scope test

--- spec/unit/rip/utilities/scope_spec.rb
+++ spec/unit/rip/utilities/scope_spec.rb
@@ -2,9 +2,12 @@ require 'spec_helper'
 
 describe Rip::Utilities::Scope do
   subject { scope_foo }
-  let(:scope_foo) { Rip::Utilities::Scope.new(:foo => 111) }
+  let(:scope_foo) { Rip::Utilities::Scope.new }
+
+  before(:each) { scope_foo[:foo] = 111 }
 
   specify { expect(scope_foo[:foo]).to be(111) }
+  specify { expect(scope_foo[:zebra]).to be_nil }
 
   context 'extending' do
     before(:each) { scope_foo[:bar] = 222 }
@@ -23,13 +26,6 @@ describe Rip::Utilities::Scope do
 
       specify { expect(scope_bar[:foo]).to be(111) }
       specify { expect(scope_bar[:bar]).to be(333) }
-    end
-
-    context 'misses' do
-      subject { nested_foo }
-      let(:nested_foo) { scope_foo.nested_context }
-
-      specify { expect(nested_foo[:zebra]).to be_nil }
     end
   end
 end
