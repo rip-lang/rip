rip-lang/rip

commit 2b98f98beb0b6d1cf546b79d24dbac5dbc2389f9

Start transforming nodes

lib/rip/nodes/character.rb
@@ -1,0 +1,18 @@
+require 'rip/nodes'
+
+module Rip::Nodes
+  class Character
+    attr_reader :data
+
+    def initialize(data)
+      @data = data.to_sym
+    end
+
+    def ==(other)
+      data == other.data
+    end
+
+    def evaluate
+    end
+  end
+end

lib/rip/ast.rb
@@ -1,4 +1,24 @@
+require 'rip/nodes/nil'
+require 'rip/transform'
+
 module Rip
-  module AST
+  class AST
+    attr_reader :tree
+
+    def initialize(parse_tree)
+      parse = if parse_tree.empty?
+        [Rip::Nodes::Nil]
+      else
+        parse_tree
+      end
+      @tree = Rip::Transform.new.apply parse
+    end
+
+    # TODO recurse tree
+    def evaluate
+      tree.each do |node|
+        puts node.inspect
+      end
+    end
   end
 end

lib/rip/nodes.rb
@@ -1,0 +1,6 @@
+require 'rip'
+
+module Rip
+  module Nodes
+  end
+end

lib/rip/nodes/comment.rb
@@ -1,0 +1,18 @@
+require 'rip/nodes'
+
+module Rip::Nodes
+  class Comment
+    attr_reader :text
+
+    def initialize(text)
+      @text = text
+    end
+
+    def ==(other)
+      text == other.text
+    end
+
+    def evaluate
+    end
+  end
+end

lib/rip/nodes/decimal.rb
@@ -1,0 +1,21 @@
+require 'rip/nodes'
+
+module Rip::Nodes
+  class Decimal
+    attr_reader :data
+    attr_reader :sign
+
+    def initialize(number, sign = :+)
+      @data = Float number
+      @sign = sign
+    end
+
+    def ==(other)
+      (data == other.data) &&
+        (sign == other.sign)
+    end
+
+    def evaluate
+    end
+  end
+end

lib/rip/nodes/integer.rb
@@ -1,0 +1,21 @@
+require 'rip/nodes'
+
+module Rip::Nodes
+  class Integer
+    attr_reader :data
+    attr_reader :sign
+
+    def initialize(number, sign = :+)
+      @data = Integer number
+      @sign = sign
+    end
+
+    def ==(other)
+      (data == other.data) &&
+        (sign == other.sign)
+    end
+
+    def evaluate
+    end
+  end
+end

lib/rip/nodes/list.rb
@@ -1,0 +1,18 @@
+require 'rip/nodes'
+
+module Rip::Nodes
+  class List
+    attr_reader :items
+
+    def initialize(*items)
+      @items = items
+    end
+
+    def ==(other)
+      other.respond_to?(:items) && (items == other.items)
+    end
+
+    def evaluate
+    end
+  end
+end

lib/rip/nodes/nil.rb
@@ -1,0 +1,9 @@
+require 'rip/nodes'
+
+module Rip::Nodes
+  class Nil
+    def self.evaluate
+      nil
+    end
+  end
+end

commit 0774c69a30a9bb5a05d1bbd18e833a1f8751c0d2

Fix potential memory leak introduced in commit 2b98f98beb

lib/rip/nodes/character.rb
@@ -4,7 +4,7 @@ module Rip::Nodes
 
     def initialize(location, data)
       super(location)
-      @data = data.to_sym
+      @data = data.to_s
     end
 
     def ==(other)