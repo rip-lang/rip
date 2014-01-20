89abbdb90a09d149f01383901177a933ce534a14
Thomas Ingram <code@ravinggenius.com>
2014-01-19 23:28:10 -0500

    Implement Rip::Core::System.class_instance
    Also include `System` in the global context and implement
    `System.require` to load Rip modules (absolute or relative to
    working directory)

--- lib/rip/core/system.rb
+++ lib/rip/core/system.rb
@@ -1,4 +1,19 @@
 module Rip::Core
   class System < Rip::Core::Base
+    define_class_instance do |class_instance|
+      class_instance['require'] = Rip::Core::RubyLambda.new(Rip::Utilities::Keywords[:dash_rocket], [
+        Rip::Nodes::Reference.new(nil, 'module_name')
+      ]) do |this, context|
+        module_name = context['module_name'].characters.map(&:data).join
+
+        Rip::Loaders::FileSystem.load_module(module_name).tap do |reply|
+          raise Rip::Exceptions::LoadException.new("Cannot load module: `#{module_name}`") if reply.nil?
+        end
+      end
+
+      def class_instance.to_s
+        'System'
+      end
+    end
   end
 end
---
+++ spec/unit/rip/core/system_spec.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+
+describe Rip::Core::System do
+  let(:class_instance) { Rip::Core::System.class_instance }
+
+  describe 'debug methods' do
+    specify { expect(class_instance.to_s).to eq('System') }
+    specify { expect(class_instance.inspect).to eq('#< System >') }
+  end
+
+  describe '.class_instance' do
+    specify { expect(class_instance).to_not be_nil }
+    specify { expect(class_instance['class']).to eq(Rip::Core::Class.class_instance) }
+  end
+
+  describe '.require' do
+    specify { expect(class_instance.symbols).to match_array(['@', 'class', 'self', 'require']) }
+    specify { expect(class_instance['require']).to be_a(Rip::Core::Lambda) }
+
+    let(:project_dir) { Pathname(Dir.pwd) }
+    let(:syntax_tree) { syntax_tree = Rip::Compiler::Parser.new(main_file, main_file.read).syntax_tree }
+    let(:final_result) { Rip::Compiler::Driver.new(syntax_tree).interpret }
+
+    before(:each) do
+      write_file('library.rip', <<-RIP)
+        'hello, world'
+      RIP
+
+      in_current_dir do
+        absolute_path = project_dir + 'library.rip'
+        write_file('absolute.rip', <<-RIP)
+          System.require('#{absolute_path}')
+        RIP
+      end
+
+      write_file('relative.rip', <<-RIP)
+        System.require('./library')
+      RIP
+
+      write_file('broken.rip', <<-RIP)
+        System.require('./not_exist')
+      RIP
+    end
+
+    describe 'requiring an absolute file' do
+      let(:main_file) { project_dir + current_dir + 'absolute.rip' }
+
+      specify do
+        in_current_dir do
+          characters = 'hello, world'.split('').map { |c| Rip::Core::Character.new(c) }
+          expect(final_result).to eq(Rip::Core::String.new(characters))
+        end
+      end
+    end
+
+    describe 'requiring a relative file' do
+      let(:main_file) { project_dir + 'relative.rip' }
+
+      specify do
+        in_current_dir do
+          characters = 'hello, world'.split('').map { |c| Rip::Core::Character.new(c) }
+          expect(final_result).to eq(Rip::Core::String.new(characters))
+        end
+      end
+    end
+
+    describe 'requiring a non-existent file' do
+      let(:main_file) { project_dir + 'broken.rip' }
+
+      specify do
+        in_current_dir do
+          expect { final_result }.to raise_error(Rip::Exceptions::LoadException)
+        end
+      end
+    end
+  end
+end
