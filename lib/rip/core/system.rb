3a0d097179aa370aaf61ef09143c5c3d464ec646
Thomas Ingram <code@ravinggenius.com>
2014-01-31 17:50:39 -0500

    Stub in commented class properties for System

--- lib/rip/core/system.rb
+++ lib/rip/core/system.rb
@@ -11,8 +11,23 @@ module Rip::Core
         end
       end
 
-      class_instance['List'] = Rip::Core::List.class_instance
-      class_instance['String'] = Rip::Core::String.class_instance
+      # class_instance['Boolean']           = Rip::Core::Boolean.class_instance
+      # class_instance['Character']         = Rip::Core::Character.class_instance
+      # class_instance['Class']             = Rip::Core::Class.class_instance
+      # class_instance['Date']              = Rip::Core::Date.class_instance
+      # class_instance['DateTime']          = Rip::Core::DateTime.class_instance
+      # class_instance['Decimal']           = Rip::Core::Decimal.class_instance
+      # class_instance['Exception']         = Rip::Core::Exception.class_instance
+      # class_instance['Integer']           = Rip::Core::Integer.class_instance
+      # class_instance['KeyValue']          = Rip::Core::KeyValue.class_instance
+      # class_instance['Lambda']            = Rip::Core::Lambda.class_instance
+      class_instance['List']              = Rip::Core::List.class_instance
+      # class_instance['Map']               = Rip::Core::Map.class_instance
+      # class_instance['Object']            = Rip::Core::Object.class_instance
+      # class_instance['Range']             = Rip::Core::Range.class_instance
+      # class_instance['RegularExpression'] = Rip::Core::RegularExpression.class_instance
+      class_instance['String']            = Rip::Core::String.class_instance
+      # class_instance['Time']              = Rip::Core::Time.class_instance
 
       def class_instance.to_s
         'System'
