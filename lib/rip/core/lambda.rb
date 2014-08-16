8543c5f0323806e7efb24c4751d878c3eb821412
Thomas Ingram <code@ravinggenius.com>
2014-08-16 18:51:13 -0400

    Refactor Rip::Core::DynamicProperty to optionally not memoize the resolved value

--- lib/rip/core/lambda.rb
+++ lib/rip/core/lambda.rb
@@ -97,13 +97,14 @@ module Rip::Core
   class DynamicProperty
     attr_reader :block
 
-    def initialize(&block)
+    def initialize(memoizable = true, &block)
+      @memoizable = memoizable
       @block = block
     end
 
     def resolve(key, receiver)
       block.call(receiver).tap do |reply|
-        receiver.properties[key] = reply
+        receiver.properties[key] = reply if @memoizable
       end
     end
   end
