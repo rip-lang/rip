708416244f2fc879903c8fb5845527d22befa6ad
Thomas Ingram <code@ravinggenius.com>
2014-08-15 23:57:38 -0400

    Ensure integration tests can find project binary

--- spec/support/aruba.rb
+++ spec/support/aruba.rb
@@ -5,4 +5,9 @@ RSpec.configure do |config|
   config.include Aruba::Api
 
   config.after(:each) { restore_env }
+
+  ENV['PATH'] = [
+    Rip.root.parent.join('bin'),
+    ENV['PATH']
+  ].join(File::PATH_SEPARATOR)
 end
