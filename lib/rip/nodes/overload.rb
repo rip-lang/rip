9871ae43e783996ef195dc008b7ec8658047928b
Thomas Ingram <code@ravinggenius.com>
2014-11-04 20:54:07 -0500

    Extract Rip::Nodes::Overload#to_debug from Rip::Nodes::Lambda#to_debug

--- lib/rip/nodes/overload.rb
+++ lib/rip/nodes/overload.rb
@@ -26,6 +26,24 @@ module Rip::Nodes
       Rip::Core::Overload.new(_parameters, body)
     end
 
+    def to_debug(level = 0)
+      parameters_debug_inner = parameters.inject([]) do |reply, parameter|
+        reply + parameter.to_debug(level + 2)
+      end
+
+      parameters_debug = [ [ level + 1, 'parameters = [' ] ] +
+        parameters_debug_inner +
+        [ [ level + 1, ']' ] ]
+
+      body_debug = [ [ level + 1, 'body = [' ] ] +
+        body.to_debug(level + 2) +
+        [ [ level + 1, ']' ] ]
+
+      [
+        [ level, "#{super.last.last}" ]
+      ] + parameters_debug + body_debug
+    end
+
     # convert list of overloads, some which may have optional paramters, into list of overloads with no optional parameters
     def self.expand(overloads)
       overloads.map do |overload|
