1a10264298e4f2cb27abb6fd6ea35784786a130c
Thomas Ingram <code@ravinggenius.com>
2014-12-14 15:58:54 -0500

    Interpret Rip::Nodes::Decimal to produce Rip::Core::Rational

--- lib/rip/core/rational.rb
+++ lib/rip/core/rational.rb
@@ -1,12 +1,31 @@
 module Rip::Core
   class Rational < Rip::Core::Base
+    attr_reader :numerator
+    attr_reader :denominator
     attr_reader :data
-    attr_reader :sign
 
-    def initialize(data, sign = :+)
+    def initialize(numerator, denominator, sign = :+)
       super()
-      @data = data
-      @sign = sign.to_sym
+
+      @numerator = numerator
+      @denominator = denominator
+      @data = Rational(numerator, denominator) * (sign.to_sym == :+ ? 1 : -1)
+
+      self['type'] = self.class.type_instance
+    end
+
+    def ==(other)
+      data == other.data
+    end
+
+    def to_s_prep_body
+      super + [ "numerator = #{numerator}, denominator = #{denominator}" ]
+    end
+
+    define_type_instance('rational') do |type_instance|
+      def type_instance.to_s
+        '#< System.Rational >'
+      end
     end
   end
 end
