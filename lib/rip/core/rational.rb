87daed2e45b961a69d60604b1263519c75216d1f
Thomas Ingram <code@ravinggenius.com>
2014-12-15 09:46:36 -0500

    Implement and test System.Rational.{+,-,*,/,%,to_string}

--- lib/rip/core/rational.rb
+++ lib/rip/core/rational.rb
@@ -23,6 +23,33 @@ module Rip::Core
     end
 
     define_type_instance('rational') do |type_instance|
+      %w[
+        + -
+        * /
+        %
+      ].each do |property|
+        type_instance[property] = Rip::Core::DelayedProperty.new do |_|
+          overload = Rip::Core::NativeOverload.new([
+            Rip::Core::Parameter.new('a', type_instance),
+            Rip::Core::Parameter.new('b', type_instance)
+          ]) do |context|
+            result = (context['a'].data.send(property, context['b'].data))
+            new(result.numerator, result.denominator)
+          end
+
+          Rip::Core::Lambda.new(Rip::Compiler::Driver.global_context.nested_context, [ overload ])
+        end
+      end
+
+      type_instance['@']['to_string'] = Rip::Core::DelayedProperty.new do |_|
+        to_string_overload = Rip::Core::NativeOverload.new([
+        ]) do |context|
+          this = context['@']
+          Rip::Core::String.from_native("(#{this.numerator} / #{this.denominator})")
+        end
+        Rip::Core::Lambda.new(Rip::Compiler::Driver.global_context.nested_context, [ to_string_overload ])
+      end
+
       def type_instance.to_s
         '#< System.Rational >'
       end
