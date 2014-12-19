30c34ced2b503911e8708d94adbedbda46ea6882
Thomas Ingram <code@ravinggenius.com>
2014-12-19 16:33:43 -0500

    Remember to expose System.Rational
    Should have been part of commit 1a102642

--- lib/rip/core/system.rb
+++ lib/rip/core/system.rb
@@ -28,6 +28,7 @@ module Rip::Core
       # type_instance['Map']               = Rip::Core::DelayedProperty.new { |_| Rip::Core::Map.type_instance }
       # type_instance['Object']            = Rip::Core::DelayedProperty.new { |_| Rip::Core::Object.type_instance }
       # type_instance['Range']             = Rip::Core::DelayedProperty.new { |_| Rip::Core::Range.type_instance }
+      type_instance['Rational']          = Rip::Core::DelayedProperty.new { |_| Rip::Core::Rational.type_instance }
       # type_instance['RegularExpression'] = Rip::Core::DelayedProperty.new { |_| Rip::Core::RegularExpression.type_instance }
       type_instance['String']            = Rip::Core::DelayedProperty.new { |_| Rip::Core::String.type_instance }
       # type_instance['Time']              = Rip::Core::DelayedProperty.new { |_| Rip::Core::Time.type_instance }
